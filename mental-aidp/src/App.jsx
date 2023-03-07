c742d9486a3fbb22c7bdb5068c22547f2265d016
Paritosh Singh <paritoshsingh1609@gmail.com>
2023-03-07 12:06:17 +0530

    initial commit of new repo

---
+++ mental-aidp/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div className='text-center bg-gumnaam h-screen flex justify-center items-center text-saaf'>
+      Kaam shuru karo doston !!!
+    </div>
+  )
+}
+
+export default App
