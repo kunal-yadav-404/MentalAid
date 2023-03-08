5b7e25e586d78849c6145b3efc3c50b893fbe4a5
Paritosh Singh <paritoshsingh1609@gmail.com>
2023-03-08 21:12:05 +0530

    adding some eventlistners

--- mental-aidp/src/pages/Detect.jsx
+++ mental-aidp/src/pages/Detect.jsx
@@ -37,18 +37,24 @@ function Detect() {
   const handleClick= ()=>{
     window.location.href='./result';
   }
- 
+  function labon(){
+    document.getElementById("reclab").classList.remove("hidden");
+  }
+  function laboff(){
+    document.getElementById("reclab").classList.add("hidden");
+  }
   return (
-    <div className='text-center h-screen'>
+    <div className='text-center h-screen '>
+    <h6 id='reclab' className='hidden pt-2'>Recording...</h6>
       <div className='flex justify-center items-center mt-16'>
         <video autoPlay muted controls id="tv"></video>
       </div>
       <div className='mx-auto my-5'>
-        <button className='px-3 arambh'><FaPlayCircle size={30}/></button>
-        <button className='px-3 viram'><FaRegStopCircle size={30}/></button>
+        <button className='bg-slate-300 hover:bg-slate-400 mr-5 rounded-full arambh' onClick={labon}><FaPlayCircle size={30}/></button>
+        <button className='bg-slate-300 hover:bg-slate-400 ml-5 rounded-full viram' onClick={laboff}><FaRegStopCircle size={30}/></button>
       </div>
       <div>
-        <button onClick={handleClick}>Analyze</button>
+        <button className='rounded-md bg-emerald-500 w-24 border-spacing-2 hover:bg-emerald-600' onClick={handleClick}>Analyze</button>
       </div>
     </div>
   )
