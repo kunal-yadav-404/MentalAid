b2dc0868332fd9314e93a537b089584f340a19f8
Paritosh Singh <paritoshsingh1609@gmail.com>
2023-05-17 13:05:48 +0530

    adding loading label on result page

--- mental-aidp/src/pages/Result.jsx
+++ mental-aidp/src/pages/Result.jsx
@@ -5,17 +5,36 @@ import "../css/dashboard.css";
 
 function Result() {
   const [res, setRes] = useState("");
+  const [loading, setLoading] = useState(true);
+
   useEffect(() => {
+    setLoading(true);
     fetch("http://localhost:3000/api")
       .then((response) => response.json())
       .then((data) => {
         setRes(data.result);
-      });
+        console.log(res);
+      }).catch((error) => {
+          console.error(error);
+        });
   }, []);
+
+  useEffect(()=>{
+    if(res!=""){
+      console.log(res);
+      setLoading(false);
+    }
+  },[res]);
 
   return (
     <>
-      <div className="box-divide"></div>
+      <div className="box-divide">
+      {loading ? (
+        <div>Loading...</div>
+      ) : (
+        <div>{res}</div>
+      )}
+      </div>
       <div>{res}</div>
       <Routes>
         <Route path="/dashboard" element={<Dashboard />} />
