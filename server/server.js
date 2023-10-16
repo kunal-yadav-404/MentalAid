010a03b6270b4cd27a55911acdec769f01e4d076
Paritosh Singh <paritoshsingh1609@gmail.com>
2023-10-16 08:59:40 +0530

    fix-securing api creds

--- server/server.js
+++ server/server.js
@@ -6,19 +6,14 @@ const cors = require("cors");
 const { google } = require("googleapis");
 const path= require("path");
 const fs=require("fs");
+const {config} = require('./config');
 
-// intializing credentials to use google drive apis
-const client_id = '823774867075-4vq0do4ntr6648foobhdnt6puundmn8a.apps.googleusercontent.com';
-const client_secret = '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';
-const rediretURI = 'https://developers.google.com/oauthplayground/';
-const api_key = 'AIzaSyDfAMqT_3UeZ-xDYEXev2z9TEAOyGdZnAo';
-const refresh_token = '1//04gxSAWVBjcieCgYIARAAGAQSNgF-L9IrLNHC5sSxXDOduGGGOpUSvBYwTAXKbdo5-1GD1vnmF5fhucGR1rlYtCA1XuuzsvDpPw';
 const oauthtoclient = new google.auth.OAuth2(
-  client_id,
-  client_secret,
-  rediretURI
+  config.client_id,
+  config.client_secret,
+  config.rediretURI
 );
-oauthtoclient.setCredentials({ refresh_token: refresh_token });
+oauthtoclient.setCredentials({ refresh_token: config.refresh_token });
 
 //initializing variable to store ID of the file that will be uploaded on google drive
 var mId = "";
